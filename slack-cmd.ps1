910c9b5ea6b726a3fa6707ebd98bfb1738505a18
Nikhil-keshri2213 <nikhilkeshri2213@gmail.com>
2026-05-01 17:37:12 +0530

    removed secret file

--- slack-cmd.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-RestMethod -Uri "https://hooks.slack.com/services/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" `
--Method Post `
--Body '{"text":"Hello, Slack it is Ai Code Review!"}' `
--ContentType "application/json"
